ba96b355e13ba0292aaaf9716d3dc87fb0902c81
Jess Zarchi <jackfz01@gmail.com>
2025-06-09 23:07:10 -0700

    Added indexing code and ball detection

--- src/conveyor.cpp
+++ src/conveyor.cpp
@@ -31,7 +31,70 @@ pros::Task Conveyor_Bottom_Task(conveyor_bottom_task);  // Create the task, this
 
 void conveyor_top_task() {
   pros::delay(2000);  // Set EZ-Template calibrate before this function starts running
+  bool is_ball = false;
+  bool last_is_ball = is_ball;
+  int is_ball_timer = 0;
+  int amount_of_balls = 0;
   while (true) {
+    last_is_ball = is_ball;
+
+    if (!is_ball) {
+      if (optical.get_proximity() >= 255) {
+        if (is_ball_timer > 50) {
+          is_ball = true;
+        }
+        is_ball_timer += ez::util::DELAY_TIME;
+      } else {
+        is_ball_timer = 0;
+      }
+    } else {
+      if (optical.get_proximity() < 255) {
+        if (is_ball_timer > 50) {
+          is_ball = false;
+        }
+        is_ball_timer += ez::util::DELAY_TIME;
+      } else {
+        is_ball_timer = 0;
+      }
+    }
+
+    if (last_is_ball == true && is_ball == false) {
+      amount_of_balls++;
+    }
+
+    printf("is ball %i   last ball: %i      amount: %i \n", (int)is_ball, (int)last_is_ball, amount_of_balls);
+
+    if (get_active_roller_state() == INTAKE) {
+      conveyor_top_antijam.stick_enable();
+
+      if (amount_of_balls == 0) {
+        conveyor_top_set(30);
+      } else {
+        conveyor_top_set(-1);
+      }
+
+    } else if (get_active_roller_state() == OUTTAKE) {
+      conveyor_top_antijam.stick_disable();
+      amount_of_balls = 0;
+      conveyor_top_set(-127);
+    } else if (get_active_roller_state() == SCORE) {
+      conveyor_top_antijam.stick_disable();
+      amount_of_balls = 0;
+      if (is_color_sorting())
+        conveyor_top_set(-127);
+      else
+        conveyor_top_set(127);
+
+    } else if (get_active_roller_state() == POOP) {
+      conveyor_top_antijam.stick_disable();
+      amount_of_balls = 0;
+      conveyor_top_set(-127);
+    } else {
+      conveyor_top_set(0);
+    }
+
+    pros::delay(ez::util::DELAY_TIME);
+
     // Run normal antijam when scoring, outtaking, pooping
     // Run stronger antijam when intaking that will delay trying to unjam
     // if (get_active_roller_state() != INTAKE) {
@@ -39,24 +102,6 @@ void conveyor_top_task() {
     // } else {
     //   conveyor_top_antijam.stick_enable();
     // }
-
-    if (get_active_roller_state() == INTAKE) {
-      conveyor_top_set(20);
-    } else if (get_active_roller_state() == OUTTAKE) {
-      conveyor_top_set(-127);
-    } else if (get_active_roller_state() == SCORE) {
-      if (is_color_sorting())
-        conveyor_top_set(-127);
-      else
-        conveyor_top_set(127);
-
-    } else if (get_active_roller_state() == POOP) {
-      conveyor_top_set(-127);
-    } else {
-      conveyor_top_set(0);
-    }
-
-    pros::delay(ez::util::DELAY_TIME);
   }
 }
 pros::Task Conveyor_Top_Task(conveyor_top_task);  // Create the task, this will cause the function to start running
